061e527552d223bffac96063311cdd478601468e
lpotvin <ludopotvin@gmail.com>
2025-09-14 16:32:57 -0400

    add uv and basic stuff to make python work properly

--- src/pastpartner/__init__.py
+++ src/pastpartner/__init__.py
@@ -1,3 +1,12 @@
-def main() -> None:
+import wave
+from importlib.resources import files
 
+from piper import PiperVoice
+
+def main() -> None:
+    file = 'fr_FR-mls-medium.onnx'
+    model = files().joinpath(voice_file)
+
+
+    voice = PiperVoice.load(voice_model)
     print("Hello from pastpartner!")
